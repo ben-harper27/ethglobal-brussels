dd39304e296f3663572404f65cd1080a5ec25da3
Ben Harper <ben@harpoon.dev>
2024-07-13 09:39:38 +0200

    change index page

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import {DynamicContextProvider} from '@dynamic-labs/sdk-react-core';
+import {EthereumWalletConnectors} from "@dynamic-labs/ethereum"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}) {
+	return (
+		<DynamicContextProvider
+			settings={{
+				environmentId: '099cc9fb-5b06-4c40-8218-4fe90f319127',
+				walletConnectors: [EthereumWalletConnectors],
+			}}>
+			<Component {...pageProps} />
+		</DynamicContextProvider>
+	);
 }
