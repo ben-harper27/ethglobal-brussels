cd49a703b53c2172992fb016686f9449c36c697f
Ben Harper <ben@harpoon.dev>
2024-07-13 22:59:54 +0200

    attempt worldcoin auth integration with Dynamic

---
+++ app/src/pages/auth/worldcoin.js
@@ -0,0 +1,26 @@
+import {useExternalAuth} from "@dynamic-labs/sdk-react-core";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+
+export default function Worldcoin() {
+  const {signInWithExternalJwt} = useExternalAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router?.query?.id_token?.toString());
+  }, [router]);
+  // try {
+  //   // `externalUserId`: User ID in the external auth system
+  //   // `externalJwt`: Raw encoded JWT issued by external auth system
+  //   const userProfile = await signInWithExternalJwt({
+  //     externalUserId,
+  //     externalJwt
+  //   });
+  //
+  //   if (userProfile) {
+  //     // You should be logged in at this point
+  //   }
+  // } catch (e: any) {
+  //   console.error('Dynamic login failed:', e);
+  // }
+}
