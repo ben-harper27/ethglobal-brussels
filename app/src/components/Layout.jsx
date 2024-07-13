cc4d1a2cff7882a6a386963a3b2ec4ee048c545f
Ben Harper <ben@harpoon.dev>
2024-07-13 21:37:49 +0200

    proposals page

---
+++ app/src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import {DynamicWidget} from '@dynamic-labs/sdk-react-core';
+
+export default function Layout({children}) {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+			<div className="absolute top-0 left-0 right-0 flex items-center justify-between p-4">
+				<div className="flex items-center">
+					<Image
+						width={128}
+						height={128}
+						alt="Logo"
+						className="w-10 h-10 rounded-xl"
+						src="/logo.png"
+					/>
+					<div className="ml-2 text-2xl font-semibold text-gray-800 dark:text-gray-200">
+						Foo
+					</div>
+				</div>
+				<div className="flex items-center">
+					<DynamicWidget/>
+				</div>
+			</div>
+			{children}
+		</div>
+	);
+}
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import {DynamicWidget, useDynamicContext} from '@dynamic-labs/sdk-react-core';
+import {useDynamicContext} from '@dynamic-labs/sdk-react-core';
 import {Inter} from "next/font/google";
 import {createSafe, sendEthToSafe} from "@/lib/safe";
 import {Button} from "@/components/ui/button";
+import Layout from "@/components/Layout";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -10,24 +10,7 @@ export default function Home() {
 	const {primaryWallet} = useDynamicContext();
 
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-			<div className="absolute top-0 left-0 right-0 flex items-center justify-between p-4">
-				<div className="flex items-center">
-					<Image
-						width={128}
-						height={128}
-						alt="Logo"
-						className="w-10 h-10 rounded-xl"
-						src="/logo.png"
-					/>
-					<div className="ml-2 text-2xl font-semibold text-gray-800 dark:text-gray-200">
-						Foo
-					</div>
-				</div>
-				<div className="flex items-center">
-					<DynamicWidget/>
-				</div>
-			</div>
+		<Layout>
 			<div className="flex flex-col items-center justify-center gap-4">
 				<div className="text-4xl font-bold text-gray-700 dark:text-gray-300">
 					Welcome to Foo
@@ -43,6 +26,6 @@ export default function Home() {
 			}}>
 				Send Eth to Safe
 			</Button>
-		</div>
+		</Layout>
 	);
 }
