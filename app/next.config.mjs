21a7f188302d48a9eae4019bc163776360734a89
Ben Harper <ben@harpoon.dev>
2024-07-14 04:43:48 +0200

    grrr

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_BASE: process.env.API_BASE,
+  },
+  images: {
+		domains: [
+			"storage.googleapis.com",
+		],
+	},
 };
 
 export default nextConfig;
